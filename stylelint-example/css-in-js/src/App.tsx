538f6bee871deaa0ecd293882a9651d415a7f2b0
dev-yakuza <dev.yakuza@gmail.com>
2022-03-06 19:21:58 +0900

    Add stylelint example

---
+++ stylelint-example/css-in-js/src/App.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+const Title = styled.h2`
+  margin: 0 0 1rem 0;
+  font-size: 1.5rem;
+`;
+
+function App() {
+  return (
+    <div>
+      <Title>Learn React</Title>
+    </div>
+  );
+}
+
+export default App;
